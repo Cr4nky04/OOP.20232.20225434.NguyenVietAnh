0728c2a7d98cee940b0129042a868a277e8cc937
Cr4nky04 <Anh.NV225434@sis.hust.edu.vn>
2024-03-25 11:21:52 +0700

    Create Lab02

---
+++ VAnh/Student.java
@@ -0,0 +1,20 @@
+package VAnh;
+
+public class Student {
+    private String name;
+    private int age;
+    private int Snum;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public Student(String s, int a)
+    {
+        name = s;
+        age = a;
+    }
+}
